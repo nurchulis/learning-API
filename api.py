eed29a42a5a4d615cc703690eaa8be1c86f8082e
nurchulis <nura3609@gmail.com>
2019-03-01 21:07:16 +0700

    Change place server.log to uploads

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ template_path = os.path.join(project_root)
 mysql = MySQL()
 app = Flask(__name__,template_folder=template_path)
 ##upload file function
-file_handler = logging.FileHandler('static/server.log')
+file_handler = logging.FileHandler('uploads/server.log')
 app.logger.addHandler(file_handler)
 app.logger.setLevel(logging.INFO)
 
